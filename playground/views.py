eed15edeaf457a4629ca7581a8f69d5ced19a960
Denis <denis.murphy@fundrock.com>
2022-03-10 07:51:28 +0000

    set up admin models

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,16 @@
+from unicodedata import decimal
+from webbrowser import get
+from django.forms import DecimalField
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.contenttypes.models import ContentType
+from django.db import transaction, connection
+from store.models import Collection, OrderItem, Product, Order
+from tags.models import TaggedItem
 
-
-def calculate():
-    x = 1
-    y = 2
-    return x
 
 
 def say_hello(request):
-    x = calculate()
+    with connection.cursor() as cursor:
+        cursor.execute()
+
     return render(request, 'hello.html', {'name': 'Mosh'})
